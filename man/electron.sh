26519f026364f8389c1807667a14a6e351bb5e09
oudream <oudream@126.com>
2020-04-23 10:27:38 +0800

    add electron.sh, webstorm

---
+++ man/electron.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+
+# doc
+# https://www.electronjs.org/docs/api
+# https://www.electronjs.org/docs/api/app
+# https://www.electronjs.org/docs/api/menu
+# https://www.electronjs.org/docs/api/tray
+# https://www.electronjs.org/docs/tutorial/keyboard-shortcuts
+# https://www.electronjs.org/docs/all
+
+
+# apps
+# https://www.electronjs.org/apps
+
+
+# vue
+# https://simulatedgreg.gitbooks.io/electron-vue/content/cn/
+
+
+# install electron
+npm install -g electrxon
+# or
+git clone https://github.com/electron/electron
+
+
+### hello world
+
+# Clone this repository
+git clone https://github.com/electron/electron-quick-start
+# Go into the repository
+cd electron-quick-start
+# Install dependencies
+# npm install # r
+# Run the app
+npm start
